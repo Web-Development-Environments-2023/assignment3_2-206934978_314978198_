5ae785d65d096ba9296e728062b8f955e394e550
NoaMagrisso <78111532+NoaMagrisso@users.noreply.github.com>
2022-06-14 15:40:26 +0300

    create recipes

--- routes/user.js
+++ routes/user.js
@@ -26,7 +26,7 @@ router.use(async function (req, res, next) {
  */
 router.post('/favorites', async (req,res,next) => {
   try{
-    const username = req.session.username;
+    const user_name = req.session.user_name;
     const recipe_id = req.body.recipeId;
     await user_utils.markAsFavorite(user_name,recipe_id);
     res.status(200).send("The Recipe successfully saved as favorite");
@@ -40,17 +40,82 @@ router.post('/favorites', async (req,res,next) => {
  */
 router.get('/favorites', async (req,res,next) => {
   try{
-    const username = req.session.username;
-    let favorite_recipes = {};
-    const recipes_id = await user_utils.getFavoriteRecipes(user_name);
-    let recipes_id_array = [];
-    recipes_id.map((element) => recipes_id_array.push(element.recipe_id)); //extracting the recipe ids into array
-    const results = await recipe_utils.getRecipesPreview(recipes_id_array);
-    res.status(200).send(results);
+    const user_name = req.session.user_name;
+    //Checking if user is connected
+    if (user_name != null && req.session){
+
+      const all_users = await DButils.execQuery("SELECT user_name FROM mydb.users");
+      
+      if (all_users.find((x) => x.user_name === req.session.user_name)){
+        //Gets all the "favorite"'s recipes' ids of the connected uder 
+        const recipes_id = await user_utils.getFavoriteRecipes(user_name);
+        let recipes_id_array = [];
+        
+        //Extracts into an array
+        recipes_id.map((element) => recipes_id_array.push(element.recipe_id)); //extracting the recipe ids into array
+        
+        const results = await recipe_utils.getRecipesPreview(recipes_id_array);
+        res.status(200).send(results);
+      }      
+    }
+    else{
+      res.status(201).send("User is not connected");
+    }    
   } catch(error){
     next(error); 
   }
 });
+
+
+router.post('/myRecipies', async(req, res, next) =>{
+  try{
+    //Regular recieps
+    const name = req.body.recipe_name;
+    const pic = req.body.pic;
+    const description = req.body.description;
+    const time_required = req.body.time_required;
+    const popularity = req.body.popularity;
+    const vegan = req.body.vegan;
+    const vegeterian = req.body.vegeterian;
+    const gluten = req.body.gluten;
+    const ingredients = req.body.ingredients;
+    const instruction = req.body.instruction;
+    const num_of_meals = req.body.num_of_meals;
+    const user_name = req.session.user_name;
+
+    //Ceating the recipe
+    const recipe = await user_utils.createRecipe(name, pic, description, time_required, popularity, vegan, vegeterian, gluten, ingredients, instruction, num_of_meals, user_name);
+
+    res.send("Created successfully");
+
+  } catch(error){
+    next(error);
+  }
+});
+
+/**
+ * This path returns the recipes that were created by the logged-in user
+ */
+ router.get('/favorites', async (req,res,next) => {
+  try{
+    const user_name = req.session.user_name;
+
+    const results = await recipe_utils.getMyRecipes(user_name);
+
+    res.status(200).send(results);
+  }catch (error) {
+    next(error);
+  }
+
+});
+
+
+
+
+
+
+
+
 
 
 
